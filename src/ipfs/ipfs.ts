2d98fe45d536ecd9d4056d1351916578d1f9f120
0xRaccoon <112493530+0xRaccoon@users.noreply.github.com>
2023-08-02 08:21:00 -0300

    feat: ipfs natspec (#37)

--- src/ipfs/ipfs.ts
+++ src/ipfs/ipfs.ts
@@ -3,6 +3,10 @@ import { IpfsApi } from '../ipfsApi';
 import { IOracle } from '../types/typechain';
 import { bytes32ToCid } from '../utils/cid';
 
+/**
+ * @title Ipfs class
+ * @notice Helper class to compose complex queries to IPFS
+ */
 export class Ipfs {
   private oracle: IOracle;
   private ipfsApi: IpfsApi;
@@ -12,6 +16,11 @@ export class Ipfs {
     this.ipfsApi = ipfsApi;
   }
 
+  /**
+   * Gets the response type for a request
+   * @param requestId - The request id to get the response type for
+   * @returns the response type for the request
+   */
   public getResponseType = async (requestId: BytesLike): Promise<string> => {
     const request = await this.oracle.getRequest(requestId);
     const cid = bytes32ToCid(request.ipfsHash);
--- src/ipfsApi.ts
+++ src/ipfsApi.ts
@@ -6,9 +6,10 @@ import { RequestMetadata } from './types/types';
 import { CONSTANTS } from './utils';
 
 /**
- * @Draft
- * Wrapper for Pinata SDK
- * Uploads metadata to ipfs and hides the sdk implementation details, that can be changed in the future
+ * @title IpfsApi class
+ * @notice Wrapper for Pinata SDK
+ * @dev Uploads metadata to IPFS, and hides the SDK implementation details
+ * and hides the sdk implementation details
  */
 export class IpfsApi {
   api: PinataClient;
@@ -20,7 +21,7 @@ export class IpfsApi {
 
   /**
    * Uploads metadata to IPFS and returns the CID as bytes32 which will be saved on-chain
-   * @dev pinata will detect if a json metadata was uploaded before and will return the already uploaded CID
+   * @dev Pinata API will detect if a json metadata was uploaded before and will return the already uploaded CID
    * @param metadata - Contains the request response type and description
    * @return CID as bytes32
    */
@@ -39,6 +40,11 @@ export class IpfsApi {
     return cidToBytes32(result.IpfsHash);
   }
 
+  /**
+   * Gets the request metadata from IPFS
+   * @param cid - the CID to retrieve the metadata from IPFS
+   * @return RequestMetadata that contains responseType and description
+   **/
   public async getMetadata(cid: string): Promise<RequestMetadata> {
     const response = await axios.get<RequestMetadata>(`${CONSTANTS.IPFS_BASE_URL}/${cid}`);
     return response.data;
